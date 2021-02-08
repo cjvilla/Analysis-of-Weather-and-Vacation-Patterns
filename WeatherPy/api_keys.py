6b5fe6d7caae106a41cd3ea60c16d25d4d6262a9
cjvilla <CelineVillareal2020@northwestern.edu>
2021-02-07 22:23:17 -0600

    fixed dataframe and determined that cities humidities > 100 is null

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "88d8aab71cfd39870cf7ea076a7246d7"
+weather_api_key = "c51718a8fe1f408977826bbb65188383"
 
 # Google API Key
 g_key = "AIzaSyCnIx-RLFV0kkjFLO7_gi-Iwafz72ae-SA"
